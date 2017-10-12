13263c7430130448643935751d4646c5a70fdf5a
ajchina <962230977@qq.com>
2017-10-12 15:02:36 +0800

    第一次提交

---
+++ sss.js
@@ -0,0 +1,6 @@
+var data = {
+    "resultcode": "200",
+    "reason": "Return Successd!",
+    "result": {"province": "重庆", "city": "", "areacode": "", "zip": "404100", "company": "联通", "card": ""},
+    "error_code": 0
+}
